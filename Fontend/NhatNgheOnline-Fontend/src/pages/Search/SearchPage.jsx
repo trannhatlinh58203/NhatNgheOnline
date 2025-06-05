f62353aea89bf7f05a199c3f3b0146154fae13ca
Your Name <you@example.com>
2025-06-05 10:11:58 +0700

    Push mã nguồn sau khi được cấp quyền collaborator tùng dương

--- Fontend/NhatNgheOnline-Fontend/src/pages/Search/SearchPage.jsx
+++ Fontend/NhatNgheOnline-Fontend/src/pages/Search/SearchPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
-import videoBg from '../../assets/videos/2videoserach.mp4';
 
 export default function SearchPage() {
   const [keyword, setKeyword] = useState('');
